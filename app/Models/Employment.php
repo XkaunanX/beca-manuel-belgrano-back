466907bacfcb8b67d222ad52a7de40dcabb169a3
XkaunanX <damipiazza11@gmail.com>
2025-09-18 17:26:24 -0300

    add last migrations

--- app/Models/Employment.php
+++ app/Models/Employment.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model
+class Employment extends Model
 {
     protected $fillable = ['name', 'type', 'income', 'description'];
     public $timestamps = true;
 
-    // Un Job pertenece a un Scholarships
+    // Un Employment pertenece a un Scholarships
     public function scholarship(){
         return $this->belongsTo(Scholarship::class);    
     }
 
-    // Un Job pertenece a un FamilyMember
+    // Un Employment pertenece a un FamilyMember
     public function familyMember(){
         return $this->belongsTo(FamilyMember::class);   
     }
